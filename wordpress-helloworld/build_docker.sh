b844b0ec212491a86a9bf815cc3a7a78fdf5de2b
louiscklaw <louiscklaw@users.noreply.github.com>
2023-03-24 23:29:23 +0800

    update,

--- wordpress-helloworld/build_docker.sh
+++ wordpress-helloworld/build_docker.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+# !/usr/bin/env bash
 
 sudo rm -rf ./gatsby/app/node_modules
 
@@ -9,3 +9,10 @@ docker compose up -d --build
 
 # docker compose exec -it gatsby bash
 docker compose logs -f
+
+# wordpress-helloworld-wordpress-1
+docker cp /var/www/html 
+
+# sudo rm -rf ./wordpress/var/www/html/*
+# sudo docker cp a9c27d021d10:/var/www/html/ ./wordpress/var/www/
+# sudo chown logic:logic -R ./wordpress/var/www
